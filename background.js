1931ab711b644c389703d0686efd4d0c278838b6
sam cladson <sam.cladson@mcmasterofceremony.com>
2020-11-26 16:46:52 +0530

    latest updates

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 chrome.tabs.onActivated.addListener((tab) => {
-  chrome.tabs.executeScript(null, { file: "./forground.js" }, () => {
-    console.log("injected");
+  chrome.tabs.get(tab.tabId, (tab_info) => {
+    console.log(tab_info.url);
+    tab_info.url === "https://www.google.com/"
+      ? (chrome.tabs.executeScript(null, { file: "./forground.js" }, () => {
+          console.log("scripts injected");
+        }),
+        chrome.tabs.insertCSS(null, { file: "./style.css" }, () => {
+          console.log("CSS injected");
+        }))
+      : null;
   });
 });
--- forground.js
+++ forground.js
@@ -1,3 +1,12 @@
-document.addEventListener("select", () => {
-  console.log("selection");
-});
+// if (document.querySelectorAll(".content-kuss-ads")) {
+//   const Adds = Array.from(document.querySelectorAll(".content-kuss-ads"));
+//   Adds.map((add) => {
+//     add.remove();
+//   });
+// }
+
+const hplogo = document
+  .querySelector("img#hplogo")
+  .classList.add("hplogo-animation");
+const manifest = (document.createElement("script").src = "./manifest.json");
+console.log(JSON.parse(manifest));
